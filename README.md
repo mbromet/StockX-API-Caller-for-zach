74a7c4546c320cba8d881985c9ce2b50ace9795e
mbromet <mbromet>
2021-08-09 01:45:35 -0500

    starting to create API to interact with the stockx data, made progress on the backend in gerneral

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 wobox stockx scraper
-Zach: you need to write out the Minimum Viable Product features, basically what we need this app to do
-script that can make api calls to stockx to collect sold product data and compile it into a .csv format
+Minimum Viable Product features: basically what we need this app to do script that can make api calls to stockx to collect sold product data and compile it into a .csv format
 
-Write out the stretch features(difficult stuff) here: instead of putting links for products you put various keywords and search stockx, ex. +Air, +Jordan, +5, +Orange, +Black 
+Stretch Features: instead of putting links for products you put various keywords and search stockx, ex. +Air, +Jordan, +5, +Orange, +Black 
+
+When running this for the first time, you may need to type in "npm i puppeteer" Note this downloades a recent version of Chromium
--- server/server.js
+++ server/server.js
@@ -11,3 +11,29 @@ app.use(express.json());
 
 app.use('./api', apiRouter);
 
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('Something went wrong'));
+
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+app.use((err, req, res, next) => { // be somewhat careful here, as this could lead to tiny security problems if this application becomes public
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
